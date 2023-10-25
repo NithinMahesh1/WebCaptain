d81ce7f25ab2aaa7230fa15307433c94bbe033c6
Nithin Mahesh <nmahesh.informatics@gmail.com>
2023-10-25 17:01:30 -0400

    Finally got OAuth2 configured and working with users subscriptions request!

--- src/main/java/org/WebCaptain/YoutubeRequests/YoutubeApiClient.java
+++ src/main/java/org/WebCaptain/YoutubeRequests/YoutubeApiClient.java
@@ -7,63 +7,67 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 public class YoutubeApiClient {
-
-    private static final String access_token = "";
-
-    public String makeRequest(String url, OAuth2AuthenticationToken authentication) throws IOException {
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+    public String makeRequest(String url, String access_token) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet subscription_request = new HttpGet(url);
-        HttpPost token_request = new HttpPost("https://oauth2.googleapis.com/token");
 
-        List<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("grant_type", "authorization_code"));
-        params.add(new BasicNameValuePair("scope", "https://www.googleapis.com/auth/youtube.readonly"));
-//        params.add(new BasicNameValuePair("redirect_uri", "http://localhost:8080/getUserSubscriptions"));
-//        params.add(new BasicNameValuePair("client_id", ""));
-//        params.add(new BasicNameValuePair("client_secret", ""));
-        params.add(new BasicNameValuePair("auth_url", "https://accounts.google.com/o/oauth2/auth"));
+        // Need to add a check for if we are sending a httpGet or httpPost
+        HttpGet requestObject = new HttpGet(url);
 
-        token_request.setEntity(new UrlEncodedFormEntity(params));
+        requestObject.addHeader("Authorization", "Bearer " +access_token);
+        HttpResponse response = httpClient.execute(requestObject);
 
-        HttpResponse response = httpClient.execute(token_request);
-        HttpEntity entity = response.getEntity();
-
-        if (entity != null) {
-            return entity.toString();
+        if (response.getEntity() != null) {
+            return EntityUtils.toString(response.getEntity());
         }
 
         return null;
     }
 
-    @Configuration
-    @EnableWebSecurity
-    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    public String retrieveUserPicture(OAuth2AuthenticationToken authentication)  {
+        Map<String, Object> authentication_attributes = Collections.EMPTY_MAP;
+        authentication_attributes = authentication.getPrincipal().getAttributes();
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests(authorizeRequests ->
-                            authorizeRequests
-                                    .antMatchers("/", "/error").permitAll()
-                                    .anyRequest().authenticated()
-                    )
-                    .oauth2Login(withDefaults());
-        }
+        return authentication_attributes.get("picture").toString();
     }
+
+//    public String makeRequest(String url, OAuth2AuthenticationToken authentication) throws IOException {
+//        CloseableHttpClient httpClient = HttpClients.createDefault();
+//        HttpGet subscription_request = new HttpGet(url);
+//        HttpPost token_request = new HttpPost("https://oauth2.googleapis.com/token");
+//
+//        List<NameValuePair> params = new ArrayList<>();
+//        params.add(new BasicNameValuePair("grant_type", "authorization_code"));
+//        params.add(new BasicNameValuePair("scope", "https://www.googleapis.com/auth/youtube.readonly"));
+//        params.add(new BasicNameValuePair("redirect_uri", "http://localhost:8080/getUserSubscriptions"));
+//        params.add(new BasicNameValuePair("client_id", ""));
+//        params.add(new BasicNameValuePair("client_secret", ""));
+//        params.add(new BasicNameValuePair("auth_url", "https://accounts.google.com/o/oauth2/auth"));
+//
+//        token_request.setEntity(new UrlEncodedFormEntity(params));
+//
+//        HttpResponse response = httpClient.execute(token_request);
+//        HttpEntity entity = response.getEntity();
+//
+//        if (entity != null) {
+//            return entity.toString();
+//        }
+//
+//        return null;
+//    }
+
 
 }
--- src/main/java/org/WebCaptain/YoutubeRequests/YoutubeController.java
+++ src/main/java/org/WebCaptain/YoutubeRequests/YoutubeController.java
@@ -1,7 +1,12 @@
 package org.WebCaptain.YoutubeRequests;
 import java.io.IOException;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,20 +22,63 @@ public class YoutubeController {
     @GetMapping("/getChannelDetails")
     public String getChannelDetails(@RequestParam String channelName, OAuth2AuthenticationToken authentication) throws IOException {
         String url = "https://www.googleapis.com/youtube/v3/search?&q=" + channelName + "&type=video&part=snippet";
-        System.out.println("This is the output from REST call: " +url);
 
-        String response = youtubeApiClient.makeRequest(url, authentication);
+        String registrationID = authentication.getAuthorizedClientRegistrationId();
+        String username = authentication.getName();
+
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(registrationID, username);
+        String access_token = client.getAccessToken().getTokenValue();
+
+        String response = youtubeApiClient.makeRequest(url, access_token);
 
         // Return the responses with channel videos list and metadata
         return response;
     }
 
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
     @GetMapping("/getUserSubscriptions")
     public String getUserSubscriptions(OAuth2AuthenticationToken authentication) throws IOException {
         String url = "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true";
-        String response = youtubeApiClient.makeRequest(url, authentication);
 
-        // Return the responses with channel videos list and metadata
+//        String registrationID = authentication.getAuthorizedClientRegistrationId();
+//        String username = authentication.getName();
+//
+//        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(registrationID, username);
+//
+//        String access_token = client.getAccessToken().getTokenValue();
+
+        // Get the registrationId (client) from the authentication
+        String registrationId = authentication.getAuthorizedClientRegistrationId();
+        // Get the name of the user
+        String userName = authentication.getName();
+
+        // Load the authorized client based on registrationId and user
+        OAuth2AuthorizedClient authorizedClient =
+                authorizedClientService.loadAuthorizedClient(registrationId, userName);
+
+        // Extract the access token
+        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+        String tokenValue = accessToken.getTokenValue();
+
+
+        String response = youtubeApiClient.makeRequest(url, tokenValue);
+
+        // Return users subscriptions to channels
+        return response;
+    }
+
+    @GetMapping("/getUserPicture")
+    public String getUserPicture(OAuth2AuthenticationToken authentication) throws IOException {
+        String response = "";
+
+        try {
+            response = youtubeApiClient.retrieveUserPicture(authentication);
+        }
+        catch(Exception err) {
+            response = err.getMessage();
+        }
+
         return response;
     }
 
