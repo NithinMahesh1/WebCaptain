ba335c3e89b37d9b7f1bc67193491d53c1fd7809
Nithin Mahesh <nmahesh.informatics@gmail.com>
2023-11-24 16:26:37 -0500

    Committing frontend app.js edits in order to properly revert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     try {
       const response = await fetch('http://localhost:8080/getChannelDetails?channelName=WhistlinDiesel');
       const jsonData = await response.json();
-
+      
       setData(jsonData);
       setLoading(false);
     } catch (error) {
@@ -27,68 +27,30 @@ function App() {
     if(loading) {
       return <p> loading .... </p>
     } 
-    else if(data && data.items) {
+    else if(data || data.items) {
       const channelResObj = data.items;
-
       console.log("we are inside renderData outputting resObj: ");
       console.log(channelResObj);
 
       return (
-        <div className='appWrapper'>
+        <div>
           {channelResObj.map((item, index) => (
-            <div key={index}>
-              
-              <div className='titleAndDesc'>
-                <img src={item.snippet.thumbnails.medium.url} alt="Thumbnail"></img>
-                <div className='ChannelDescription'>{item.snippet.title}</div>
-              </div>
-
-              <div className='titleAndPubDate'>
-                <div className='ChannelTitle'>{item.snippet.channelTitle}</div>
-                <div className='PublishTime'>{publishDateConverter(item)}</div>
-              </div>
-
-            </div>
-          ))}
+            <div key={index}>{item.snippet.description}</div>
+          ))};
         </div>
-        
       );
     }
     else {
-      return <p> No data is available .... </p>;
+      return <p> No data is available .... </p>
     }
   };
-
-  const publishDateConverter = (item) => {
-    console.log("date converter");
-    console.log(item);
-
-    if(!item || !item.snippet || !item.snippet.publishTime) {
-      return "No data available"
-    }
-    // 2023-08-18T22:02:53Z
-    const publishTime = item.snippet.publishTime;
-    const pubDate = new Date(publishTime);
-    const currentDate = new Date();
-
-    var oneDay = 1000 * 60 * 60 * 24;
-    var calculateDiff = Math.round(currentDate.getTime() - pubDate.getTime()) / (oneDay);
-    var roundedVal = calculateDiff.toString().charAt(0);
-    var res = roundedVal + " days ago";
-
-    if(roundedVal === "1") {
-      res = roundedVal + " day ago";
-    }
-
-    return res;
-  }
 
   return (
     <div className="App">
       <header className="App-header">
         
 
-        <div>{renderData()}</div>
+      <div>{renderData()}</div>
 
       </header>
     </div>
