a600373c7cc1e56f00574b769bf6ce06e01b0dd0
Nithin Mahesh <nmahesh.informatics@gmail.com>
2024-07-13 13:18:01 -0400

    Created client application worker service template

---
+++ client/WebCaptainService/Program.cs
@@ -0,0 +1,10 @@
+using WebCaptainService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
